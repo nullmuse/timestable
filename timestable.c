9ca154c9631c661c898a8b45e171cb7b68cefdc1
nullmuse <dylan.liebgott@gmail.com>
2016-10-21 16:34:56 +0000

    Add draw function

--- timestable.c
+++ timestable.c
@@ -23,9 +23,12 @@ int main(int argc, char *argv[]) {
        
       }
   }
-   int maxer = transmute_char(max); 
-   printf("%i\n",transmute_char(max)); 
-   printf("%s\n",transmute_int(maxer));
+//   int maxer = transmute_char(max); 
+//   printf("%i\n",transmute_char(max));
+//   printf("min %i\n",transmute_char(min)); 
+//   printf("min %s\n",transmute_int(1)); 
+//   printf("%s\n",transmute_int(maxer));
+   draw(min,max); 
    return 0; 
 
 }
@@ -56,5 +59,49 @@ char *transmute_int(int subject) {
    return new; 
 }
 
+
+int string_graft(char *patient, char *graft, int step) {
+   int len = strlen(graft); 
+   int i = 0; 
+   int index = 0; 
+   for(;i < len;++i) {  
+      patient[index++] = graft[i];  
+   } 
+   return step > (len + 1) ? step + 1 : (step + 1);   
+}    
+   
+
+int draw(char *min, char *max) { 
+   int start = transmute_char(min); 
+   int stop = transmute_char(max); 
+   int tmp,placer,i = 0;
+   char *buf = NULL;  
+   int size = stop * stop;  
+   int step = strlen(transmute_int(size + 1)); 
+   char *placement = malloc(size + 1); 
+   memset(placement,' ',size);
+   placement[size + 1] = '\0';
+   placer = 0;
+   placer += string_graft((placement + placer),"*",step);
+   for(i = start; i <= stop; ++i) {  
+      placer += string_graft((placement + placer),transmute_int(i),step);   
+   }
+   placement[placer] = '\0';
+   printf("%s\n",placement);
+   for(i = start;i <= stop; ++i) {
+      placer = 0; 
+      memset(placement,' ',size - 1); 
+      placer += string_graft((placement + placer),transmute_int(i),step);  
+      for(tmp = start; tmp <= stop; ++tmp) {
+         placer += string_graft((placement + placer),transmute_int(tmp * i),step);
+      } 
+      placement[placer] = '\0';
+      printf("%s\n",placement); 
+      }
+
+   return 0; 
+
+}
+          
 
 
